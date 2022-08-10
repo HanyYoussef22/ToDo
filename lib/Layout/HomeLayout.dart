63e14b4fd567ee879f8f634908591b4cc64bd607
dell <hanyy81667@gmail.com>
2022-08-11 00:01:06 +0200

    master

---
+++ lib/Layout/HomeLayout.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class HomeLayout extends StatelessWidget {
+static const String roudeName='Home';
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
